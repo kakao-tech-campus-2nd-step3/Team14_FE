34494bcb33abcdeef84eade313afe91fb1191e51
minji2219 <200146@jnu.ac.kr>
2024-09-19 21:50:16 +0900

    chore: prettier 및 craco로 절대경로 설정

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+const { CracoAliasPlugin } = require('react-app-alias');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAliasPlugin,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: './tsconfig.paths.json',
+      },
+    },
+  ],
+};
