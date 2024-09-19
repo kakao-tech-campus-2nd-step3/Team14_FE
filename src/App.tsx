a44aa059388438ad6e6b2daedca69ebddfd8695b
minji2219 <200146@jnu.ac.kr>
2024-09-19 21:50:47 +0900

    chore: 폴더 설정 및 불필요한 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          안녕 애드라 화이팅.
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">안녕 애드라 화이팅.</div>;
 }
 
 export default App;
