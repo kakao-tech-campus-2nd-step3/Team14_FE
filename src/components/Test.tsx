8c667d89366d513b5fdc43e91b8f1a9168b7f6ef
minji2219 <200146@jnu.ac.kr>
2024-09-19 21:50:47 +0900

    chore: 폴더 설정 및 불필요한 코드 삭제

---
+++ src/components/Test.tsx
@@ -0,0 +1,9 @@
+const Test = () => {
+  return (
+    <div>
+      <div>안녕하세요.</div>
+    </div>
+  );
+};
+
+export default Test;
---
+++ src/pages/test.tsx
@@ -0,0 +1,12 @@
+import Test from '@components/Test';
+import React from 'react';
+
+const test = () => {
+  return (
+    <div>
+      <Test />
+    </div>
+  );
+};
+
+export default test;
