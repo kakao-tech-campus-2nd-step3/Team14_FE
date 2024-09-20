54fac8ec451e010bbac5f7c1bd3dc20a1d5d7bfa
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-09-20 16:27:31 +0900

    feat: 주문내역 리스트 아이템이 보이도록 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import OrderListItem from '@components/OrderListItem';
 
 function App() {
-  return <div className="App">안녕 애드라 화이팅.</div>;
+  return (
+    <div className="App">
+      안녕 애드라 화이팅.
+      <OrderListItem />
+    </div>
+  );
 }
 
 export default App;
