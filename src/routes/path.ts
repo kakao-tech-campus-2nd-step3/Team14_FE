6b471a58e4cf42a42f55aa14cb7a8c37fbba3661
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-09-26 16:24:13 +0900

    feat: 페이지 경로 추가

--- src/routes/path.ts
+++ src/routes/path.ts
@@ -1,11 +1,11 @@
 export const RouterPath = {
-    introtuce: '/introtuce',
-    main : '/main',
-    login : '/login',
-    signup : '/signup',
-    myPage : '/mypage',
-    myPageOrderHistory : '/mypage/orderhistory',
-    myPageOrderDetail : '/mypage/orderdetail',
-    myPagePoint : '/mypage/point',
-    payment: '/payment'
+  introtuce: '/',
+  main: '/main',
+  login: '/login',
+  signup: '/signup',
+  myPage: '/mypage',
+  myPageOrderHistory: '/mypage/orderhistory',
+  myPageOrderDetail: '/mypage/orderdetail',
+  myPagePoint: '/mypage/point',
+  payment: '/payment',
 };
