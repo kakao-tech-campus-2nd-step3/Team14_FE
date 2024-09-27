2eaaa672f95cbc3d0a6cab0293f7e48e79aff095
dlawlghks <ikuwlghks@naver.com>
2024-09-27 16:43:39 +0900

    feat: global styles에 yellow 색상 및 z-index 값 추가
    - 새로운 yellow 색상을 추가했습니다.
    - header와 dropdown에 사용할 z-index 값을 추가하여 레이어 관리가 용이하도록 설정했습니다.

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -2,5 +2,10 @@ export const Common = {
   colors: {
     primary: '#059770',
     primary05: 'rgba(5,151,112,.5)',
+    yellow: '#F1CF06',
+  },
+  zIndex: {
+    header: 9999,
+    dropdown: 1000,
   },
 };
