9af72346a7345856428f4b91b2810db1faed0415
minji2219 <200146@jnu.ac.kr>
2024-09-27 22:56:50 +0900

    feat: mock데이터 생성

---
+++ src/components/spot/swiper/data.ts
@@ -0,0 +1,26 @@
+export const storeList = [
+  {
+    key: 1,
+    category: '토스트',
+    title: '이삭토스트',
+    address: '경기 용인시 수지구 용구대로 2770',
+  },
+  {
+    key: 2,
+    category: '족발/보쌈',
+    title: '장충동 왕족발',
+    address: '경기 용인시 수지구 용구대로 2770',
+  },
+  {
+    key: 3,
+    category: '햄버거',
+    title: '버거킹',
+    address: '경기 용인시 수지구 용구대로 2770',
+  },
+  {
+    key: 4,
+    category: '햄버거',
+    title: '맥도날드',
+    address: '경기 용인시 수지구 용구대로 2770',
+  },
+];
