14b4f97f7bc84580e640dd2e4e8f72b82244e384
dlawlghks <ikuwlghks@naver.com>
2024-10-02 20:52:27 +0900

    refactor: GlobalStyle 적용
    - GlobalStyle을 전역으로 적용하여 스타일 통합

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { GlobalStyle } from '@styles/globalStyle';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
 );
