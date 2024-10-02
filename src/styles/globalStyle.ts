4aeeb0ee1b67175a795ab05def828ccb7f92d77c
dlawlghks <ikuwlghks@naver.com>
2024-10-02 20:51:51 +0900

    feat: Paperlogy 폰트 글로벌 스타일 추가
    - Paperlogy 폰트(Bold, Regular, Medium, Light, Extra) 파일을 추가
    - GlobalStyle에 각 폰트를 위한 @font-face 규칙 정의
    - 글로벌 스타일에서 폰트를 사용 가능하도록 설정

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,3 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+import PaperlogyBold from '../fonts/PaperlogyBold.ttf';
+import PaperlogyRegular from '../fonts/PaperlogyRegular.ttf';
+import PaperlogyMedium from '../fonts/PaperlogyMedium.ttf';
+import PaperlogyLight from '../fonts/PaperlogyLight.ttf';
+import PaperlogyExtra from '../fonts/PaperlogyExtraBold.ttf';
+
 export const Common = {
   colors: {
     primary: '#059770',
@@ -10,3 +17,40 @@ export const Common = {
     dropdown: 1000,
   },
 };
+
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'PaperlogyBold';
+    src: local('PaperlogyBold');
+    font-style: normal;
+    src: url(${PaperlogyBold}) format('truetype');
+  }
+
+  @font-face {
+    font-family: 'PaperlogyRegular';
+    src: local('PaperlogyRegular');
+    font-style: normal;
+    src: url(${PaperlogyRegular}) format('truetype');
+  }
+
+  @font-face {
+    font-family: 'PaperlogyMedium';
+    src: local('PaperlogyMedium');
+    font-style: normal;
+    src: url(${PaperlogyMedium}) format('truetype');
+  }
+
+  @font-face {
+    font-family: 'PaperlogyLight';
+    src: local('PaperlogyLight');
+    font-style: normal;
+    src: url(${PaperlogyLight}) format('truetype');
+  }
+
+  @font-face {
+    font-family: 'PaperlogyExtra';
+    src: local('PaperlogyExtra');
+    font-style: normal;
+    src: url(${PaperlogyExtra}) format('truetype');
+  }
+`;
