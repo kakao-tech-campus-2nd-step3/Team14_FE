b536cbcb3dccdafed8622821b9c8ddcaf6a058d3
minji2219 <200146@jnu.ac.kr>
2024-10-04 02:27:40 +0900

    feat: 지도에 마커 찍어보기
    지도 위에 올라갈 마커 MapMarker를 이용해 찍어보기

--- src/components/spot/map/index.tsx
+++ src/components/spot/map/index.tsx
@@ -1,6 +1,6 @@
 import { LocationContext } from '@provider/Location';
 import { useContext } from 'react';
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 const KakaoMap = () => {
   const { location } = useContext(LocationContext);
@@ -10,7 +10,14 @@ const KakaoMap = () => {
       center={{ lat: location.lat, lng: location.lng }}
       style={{ width: '100%', height: '100%' }}
       level={3}
-    />
+    >
+      <MapMarker
+        position={{
+          lat: location.lat,
+          lng: location.lng,
+        }}
+      />
+    </Map>
   );
 };
 export default KakaoMap;
