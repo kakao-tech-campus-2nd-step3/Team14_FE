19ad7783b47c5f95b8b8e31d8883375c1ba93725
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-10-10 02:24:38 +0900

    feat: 포인트 페이지 구현

--- src/pages/PointPage/index.tsx
+++ src/pages/PointPage/index.tsx
@@ -1,12 +1,35 @@
 import styled from '@emotion/styled';
 
 import Menubar from '@components/mypage/Menubar';
+import PointListItem from '@components/point/PointListItem';
+import Button from '@components/common/Button/Button';
+
+import { pointData } from '@components/point/data';
 
 const PointPage = () => {
   return (
     <Wrapper>
       <InnerWrapper>
         <Menubar />
+        <PaymentBox>
+          <Button label="충전하기" radius="20px" />
+          <Space />
+          <Button label="환전하기" radius="20px" />
+        </PaymentBox>
+        <FilterBox>
+          <Button label="충전" radius="20px" />
+          <Space />
+          <Button label="결제" radius="20px" />
+          <Space />
+          <Button label="환전" radius="20px" />
+        </FilterBox>
+        <PointList>
+          <PointListItem
+            date={pointData.date}
+            point={pointData.point}
+            filter={pointData.filter}
+          />
+        </PointList>
       </InnerWrapper>
     </Wrapper>
   );
@@ -22,4 +45,33 @@ const Wrapper = styled.div`
 
 const InnerWrapper = styled.div`
   width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const PaymentBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
+
+const FilterBox = styled.div`
+  width: 80%;
+  display: flex;
+`;
+
+const PointList = styled.div`
+  position: relative;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
+  margin: 20px 0 20px;
+`;
+
+const Space = styled.div`
+  width: 15px;
 `;
