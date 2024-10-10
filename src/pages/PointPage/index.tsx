8f9c4c1afd7f8e41daea79bce966c627f426a171
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-10-11 01:41:40 +0900

    feat: 포인트 페이지 filter기능 구현

--- src/pages/PointPage/index.tsx
+++ src/pages/PointPage/index.tsx
@@ -4,9 +4,19 @@ import Menubar from '@components/mypage/Menubar';
 import PointListItem from '@components/point/PointListItem';
 import Button from '@components/common/Button/Button';
 
-import { pointData } from '@components/point/data';
+import { pointDataSet } from '@components/point/data';
+import { useState } from 'react';
 
 const PointPage = () => {
+  const [pointFilterValue, setPointFilterValue] = useState('충전');
+
+  const changePointFilter = (filter: string) => {
+    setPointFilterValue(filter);
+  };
+  const filteredPointData = pointDataSet.filter(
+    (pointData) => pointData.filter === pointFilterValue,
+  );
+
   return (
     <Wrapper>
       <InnerWrapper>
@@ -17,18 +27,32 @@ const PointPage = () => {
           <Button label="환전하기" radius="20px" />
         </PaymentBox>
         <FilterBox>
-          <Button label="충전" radius="20px" />
+          <Button
+            label="충전"
+            radius="20px"
+            onClick={() => changePointFilter('충전')}
+          />
           <Space />
-          <Button label="결제" radius="20px" />
+          <Button
+            label="결제"
+            radius="20px"
+            onClick={() => changePointFilter('결제')}
+          />
           <Space />
-          <Button label="환전" radius="20px" />
+          <Button
+            label="환전"
+            radius="20px"
+            onClick={() => changePointFilter('환전')}
+          />
         </FilterBox>
         <PointList>
-          <PointListItem
-            date={pointData.date}
-            point={pointData.point}
-            filter={pointData.filter}
-          />
+          {filteredPointData.map((pointData) => (
+            <PointListItem
+              date={pointData.date}
+              point={pointData.point}
+              filter={pointData.filter}
+            />
+          ))}
         </PointList>
       </InnerWrapper>
     </Wrapper>
