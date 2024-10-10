ed4901dd83e8ccb69f04922039555841f1e495c6
minji2219 <200146@jnu.ac.kr>
2024-10-11 01:55:04 +0900

    feat: spot페이지 좌측 구현

--- src/pages/SpotPage/index.tsx
+++ src/pages/SpotPage/index.tsx
@@ -1,7 +1,6 @@
-import Modal from '@components/common/Modal';
-import AlertDialog from '@components/common/Modal/AlertDialog';
 import { HEADER_HEIGHT } from '@components/features/Layout/Header';
 import KakaoMap from '@components/spot/map';
+import StoreList from '@components/spot/storelist';
 import Swiper from '@components/spot/swiper';
 import styled from '@emotion/styled';
 import { Common } from '@styles/globalStyle';
@@ -9,7 +8,16 @@ import { Common } from '@styles/globalStyle';
 const SpotPage = () => (
   <Wrapper>
     <LeftWrapper>
+      <TitleWrapper>
+        <Title>마. 감. 임. 박. !</Title>
+        <SubTitle>
+          주문 마감 <Impact>30분</Impact> 전!
+        </SubTitle>
+      </TitleWrapper>
       <Swiper />
+      <Line />
+      {/* TODO: 카테고리 */}
+      <StoreList />
     </LeftWrapper>
     <KakaoMap />
   </Wrapper>
@@ -27,4 +35,29 @@ const Wrapper = styled.div`
 const LeftWrapper = styled.div`
   background-color: ${Common.colors.yellowBg};
   width: 35%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TitleWrapper = styled.div`
+  margin: 30px 0;
+`;
+
+const Title = styled.div`
+  font-family: PaperlogyBold;
+  font-size: 40px;
+`;
+
+const SubTitle = styled.div`
+  font-size: 30px;
+`;
+
+const Impact = styled.span`
+  color: red;
+`;
+
+const Line = styled.hr`
+  border: 1px dashed ${Common.colors.lightGray};
+  width: 100%;
 `;
