84e75c61e637c5af1eb586a4143d48e1e130a80c
minji2219 <200146@jnu.ac.kr>
2024-10-11 15:28:25 +0900

    feat: 스팟 추가 버튼 생성
    *기능 연결 필요

--- src/components/spot/map/index.tsx
+++ src/components/spot/map/index.tsx
@@ -1,6 +1,9 @@
 import { LocationContext } from '@provider/Location';
 import { useContext } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { BsPlusCircleFill } from 'react-icons/bs';
+import styled from 'styled-components';
+import { Common } from '@styles/globalStyle';
 
 const KakaoMap = () => {
   const { location } = useContext(LocationContext);
@@ -11,6 +14,9 @@ const KakaoMap = () => {
       style={{ width: '100%', height: '100%' }}
       level={3}
     >
+      <PlusBtn>
+        <BsPlusCircleFill size="50" />
+      </PlusBtn>
       <MapMarker
         position={{
           lat: location.lat,
@@ -21,3 +27,11 @@ const KakaoMap = () => {
   );
 };
 export default KakaoMap;
+
+const PlusBtn = styled.div`
+  position: absolute;
+  right: 20px;
+  top: 10px;
+  z-index: ${Common.zIndex.common};
+  cursor: pointer;
+`;
